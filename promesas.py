0d1e949e96ae6eb27703570b7341432daa1a74ef
UpsetPelican <u.coria@outlook.com>
2017-05-10 15:22:50 -0500

    cleanup shit

--- promesas.py
+++ promesas.py
@@ -4,14 +4,3 @@ promesas_list = ['El aborto será legal hasta los 112 meses en todo el país.',
         'Negociaremos que "EL MURO" nos separe de chiapas y sudamerica.',
         'Habrá animalitos bebé en todas las oficinas.',
         'Habrá pena de muerte para violadores y asesinos.']
-
-#choice = random.choice(promesas_list)
-#class campaign():
-#    def __init__(self, promis=None):
-#        self.promis = promesa_random()
-
-#    def promesa_random():
-#        random_choice = random.choice(promesas_list)
-#        return random_choice
-
-#    self.promis = promesa_random()
