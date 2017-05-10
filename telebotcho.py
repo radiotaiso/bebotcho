cec4d05dc70e7b4abe53fbef61dd469eacb3eba0
UpsetPelican <u.coria@outlook.com>
2017-05-10 09:38:29 -0500

    tunning

--- telebotcho.py
+++ telebotcho.py
@@ -3,17 +3,20 @@ import time
 import telepot
 from telepot.loop import MessageLoop
 import config
+from pprint import pprint
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
-    print(msg['text'])
+    pprint(msg)
 
     if content_type == 'text':
         if 'hola bebotcho' in msg['text'].lower():
             bot.sendMessage(chat_id,'que pedal?')
+        elif 'buenos dias bebotcho' in msg['text'].lower():
+            bot.sendMessage(chat_id, 'buenos días '+msg['username'])
         elif 'chinga tu madre bebotcho' in msg['text'].lower():
-            bot.sendMessage(chat_id,'la tuya en vinagre!')
+            bot.sendMessage(chat_id, 'la tuya en vinagre')
     else: 
         print('not text, srry')
 
