bdc4ffd7c3a387ab068c9a7ecffc0d33fca59306
UpsetPelican <u.coria@outlook.com>
2017-05-10 12:49:41 -0500

    fail 2

--- telebotcho.py
+++ telebotcho.py
@@ -5,6 +5,7 @@ from telepot.loop import MessageLoop
 import config
 from pprint import pprint
 from foaas import fuck
+import promesas
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -16,11 +17,20 @@ def handle(msg):
             bot.sendMessage(chat_id,'que tranza con Carranza? {}'.format(msg['from']['first_name']))
         elif 'buenos d' in msg['text'].lower():
             bot.sendMessage(chat_id, 'buenos días {}'.format(msg['from']['first_name']))
-        elif 'chingas' or 'chinga' and 'bebotcho' in msg['text'].lower():
-            response = fuck.you(name=msg['from']['first_name'], from_='Bebotcho').text
-            bot.sendMessage(chat_id, response)
-    else: 
+#        elif 'fuck you bebotcho' or 'fuck u bebotcho' in msg['text'].lower():
+#            fu = fuck.you(name=msg['from']['first_name'], from_='Bebotcho').text
+#            bot.sendMessage(chat_id, fu)
+        elif 'bebotcho fml' in msg['text'].lower():
+            fml = fuck.life(from_=msg['from']['first_name']).text
+            bot.sendMessage(chat_id, fml)
+        elif 'uriel' and 'presidente' in msg['text'].lower():
+            rand_promis = campaign.promis
+            bot.sendMessage(chat_id, 'Cuando Uriel sea presidente {}'.format(rand_promis))
+
+    elif content_type != 'text': 
         print('not text, srry')
+    else:
+        print('wtf this is bad')
 
 TOKEN = config.token
 
