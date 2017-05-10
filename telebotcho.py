89e96215966cc9396dbe4c559dd953206f7039af
UpsetPelican <u.coria@outlook.com>
2017-05-10 08:06:39 -0500

    bebocho is alive!!

--- telebotcho.py
+++ telebotcho.py
@@ -6,9 +6,15 @@ from telepot.loop import MessageLoop
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
+    print(msg['text'])
 
     if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
+        if 'hola bebotcho' in msg['text'].lower():
+            bot.sendMessage(chat_id,'que pedal?')
+        elif 'chinga tu madre bebotcho' in msg['text'].lower():
+            bot.sendMessage(chat_id,'la tuya en vinagre!')
+    else: 
+        print('not text, srry')
 
 TOKEN = ('261581634:AAGlaLIojN6LhraMxV6cvrIK34_poE8Xsu8')
 
