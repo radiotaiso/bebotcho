3654207af4e6d8ba26d520eae851c5a83df02ed6
Uriel Coria <u.coria@outlook.com>
2019-09-27 10:21:30 -0500

    Delete

--- promesas.py
+++
@@ -1,6 +0,0 @@
-promesas_list = ['El aborto será legal hasta los 112 meses en todo el país.',
-        'Pondremos anticonceptivos en el agua potable.',
-        'Nadie trabajará los lunes en mi gabinete.',
-        'Negociaremos que "EL MURO" nos separe de chiapas y sudamerica.',
-        'Habrá animalitos bebé en todas las oficinas.',
-        'Habrá pena de muerte para violadores y asesinos.']
